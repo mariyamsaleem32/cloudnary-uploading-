5caa8c8faf99b8d2a7d8447eab32e16390a4bd82
mariyamsaleem32 <mariyamsaleem87@gmail.com>
2024-11-22 15:02:16 +0500

    completed

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { db, collection, addDoc, doc, onSnapshot, query, Timestamp } from "./firebase.js";
+
 const cloudName = 'dml17obhm';
 const unsignedUploadPreset = 'ml_default';
 
@@ -10,12 +12,12 @@ fileSelect.addEventListener("click", function(e) {
   if (fileElem) {
     fileElem.click();
   }
-  e.preventDefault(); // prevent navigation to "#"
+  e.preventDefault();
 }, false);
 
 urlSelect.addEventListener("click", function(e) {
-  uploadFile('https://res.cloudinary.com/dml17obhm/image/upload/sample.jpg');
-  e.preventDefault(); // prevent navigation to "#"
+  uploadFile('https://res.cloudinary.com/hzxyensd5/image/upload/sample.jpg');
+  e.preventDefault();
 }, false);
 
 // ************************ Drag and drop ***************** //
@@ -48,7 +50,7 @@ function uploadFile(file) {
   const url = `https://api.cloudinary.com/v1_1/${cloudName}/upload`;
   const fd = new FormData();
   fd.append('upload_preset', unsignedUploadPreset);
-  fd.append('tags', 'browser_upload'); // Optional - add tags for image admin in Cloudinary
+  fd.append('tags', 'browser_upload');
   fd.append('file', file);
 
   fetch(url, {
@@ -56,27 +58,53 @@ function uploadFile(file) {
     body: fd,
   })
     .then((response) => response.json())
-    .then((data) => {
-      // File uploaded successfully
-      const url = data.secure_url;
-      console.log(data);
-      const transformURL = url.replace('/upload/','/upload/w_150,c_scale/')
-      // Create a thumbnail of the uploaded image, with 150px width
-    //   const tokens = url.split('/');
-    //   tokens.splice(-3, 0, 'w_150,c_scale');
-      const img = new Image();
-      img.src = transformURL
-      img.alt = data.public_id;
-      document.getElementById('gallery').appendChild(img);
+    .then(async(data) => {
+      console.log("data", data);
+      const docRef = await addDoc(collection(db, "images"), {
+        timestamp: Timestamp.now(),
+        name: data.display_name,
+        url: data.secure_url,
+      });
+      console.log("ID is :", docRef.id);
     })
     .catch((error) => {
       console.error('Error uploading the file:', error);
     });
 }
 
+const getData = () => {
+    const q = query(collection(db, "images"));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        console.log("changes", change.type);
+      });
+    });
+  
+    onSnapshot(q, (querySnapshot) => {
+      // Empty the gallery before adding new images
+      const gallery = document.getElementById("gallery");
+      gallery.innerHTML = '';
+  
+      querySnapshot.forEach((doc) => {
+        const imageUrl = doc.data().url;
+  
+        // Apply transformation to the URL
+        const transformURL = imageUrl.replace('/upload/', '/upload/w_150,c_scale/');
+  
+        if (imageUrl) {
+          gallery.innerHTML += `<img src="${transformURL}" />`; // Use transformed URL
+        }
+        console.log("doc data is", doc.data());
+      });
+    });
+  }
+getData();
+
 // *********** Handle selected files ******************** //
-const handleFiles = function(files) {
+const handleFiles = (files) => {
   for (let i = 0; i < files.length; i++) {
-    uploadFile(files[i]); // call the function to upload the file
+    uploadFile(files[i]);
   }
 };
+
+window.handleFiles = handleFiles;
---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getFirestore,collection, addDoc,doc, onSnapshot, query, Timestamp ,   }  from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDxpJS79rw1ywSr3v_jS-SRM-gipnVz8B0",
+  authDomain: "fir-project-526ce.firebaseapp.com",
+  projectId: "fir-project-526ce",
+  storageBucket: "fir-project-526ce.firebasestorage.app",
+  messagingSenderId: "279323963167",
+  appId: "1:279323963167:web:3f6590f9fe99cddc6b4a2a"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export{app,db,collection, addDoc,doc, onSnapshot, query, Timestamp ,  }
